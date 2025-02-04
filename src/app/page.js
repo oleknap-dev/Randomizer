6e0e7980e0fb8f624e0c3903188f473030b670f3
oleknap-dev <aleksander.napiorkowski.an@gmail.com>
2025-02-04 15:59:15 +0100

    Add buttons to change range for random number and handle invalid range

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,16 @@ import { useState } from "react";
 
 export default function Page() {
   const [number, setNumber] = useState();
+  const [max, setMax] = useState(10);
+  const [min, setMin] = useState(0);
 
   async function randomize() {
+    let newmax = max;
+    if (min >= max) {
+      setMax(min + 1);
+      newmax = min + 1;
+    }
+
     const response = await fetch("https://api.random.org/json-rpc/4/invoke", {
       method: "POST",
       headers: {
@@ -17,8 +25,8 @@ export default function Page() {
         params: {
           apiKey: "845658bd-ca8c-4406-842f-a364b43c5f6a",
           n: 1,
-          min: 1,
-          max: 10,
+          min: min,
+          max: newmax,
           base: 10,
         },
         id: 6105,
@@ -31,6 +39,12 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center">
+      <button onClick={() => setMax(max + 1)}>increase</button>
+      <h1>Max: {max}</h1>
+      <button onClick={() => setMax(max - 1)}>decrease</button>
+      <button onClick={() => setMin(min + 1)}>increase</button>
+      <h1>Min: {min}</h1>
+      <button onClick={() => setMin(min - 1)}>decrease</button>
       <button onClick={randomize}>random</button>
       <h1>{number}</h1>
     </div>
