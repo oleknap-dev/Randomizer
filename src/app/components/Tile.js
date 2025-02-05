3004cef8b7652308e4e7b4376737c8590591b62f
oleknap-dev <aleksander.napiorkowski.an@gmail.com>
2025-02-05 16:53:16 +0100

    Add Tile styling

--- src/app/components/Tile.js
+++ src/app/components/Tile.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
 
-export default function Tile({ label, href }) {
+export default function Tile({ label, href, image }) {
   return (
-    <div>
-      <Link href={href}>
-        <div>{label}</div>
+    <div className="flex relative items-center justify-center  w-80 h-48 rounded-md border border-black border-opacity-15 overflow-hidden">
+      <div
+        className="w-80 h-48 bg-center bg-cover"
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
+      <Link
+        className="w-40 h-14 absolute font-bold bg-white flex items-center rounded-md justify-center 
+        bg-opacity-60 hover:bg-opacity-80 active:bg-opacity-60 
+        border border-black border-opacity-35 hover:border-opacity-45 active:border-opacity-45 transition duration-200 ease-in-out hover:scale-[102%] "
+        href={href}
+      >
+        {label}
       </Link>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,12 @@ import Tile from "./components/Tile";
 
 export default function Page() {
   return (
-    <div>
-      <Tile label="Random Number" href="/random-number" />
+    <div className="flex flex-col justify-center items-center">
+      <Tile
+        label="Random Number"
+        href="/random-number"
+        image="/images/random-numbers.jpg"
+      />
     </div>
   );
 }
