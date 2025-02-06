e273abea3dade89f1f39564996af48ee5124fe66
oleknap-dev <aleksander.napiorkowski.an@gmail.com>
2025-02-06 14:52:46 +0100

    Implement error handling with API

--- src/app/coin-flip/CoinFlip.js
+++ src/app/coin-flip/CoinFlip.js
@@ -6,30 +6,35 @@ export default function CoinFlip() {
   const [result, setResult] = useState();
 
   async function randomize() {
-    const response = await fetch("https://api.random.org/json-rpc/4/invoke", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        method: "generateIntegers",
-        params: {
-          apiKey: "845658bd-ca8c-4406-842f-a364b43c5f6a",
-          n: 1,
-          min: 1,
-          max: 2,
-          base: 10,
+    try {
+      const response = await fetch("https://api.random.org/json-rpc/4/invoke", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        id: 6105,
-      }),
-    });
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          method: "generateIntegers",
+          params: {
+            apiKey: "845658bd-ca8c-4406-842f-a364b43c5f6a",
+            n: 1,
+            min: 1,
+            max: 2,
+            base: 10,
+          },
+          id: 6105,
+        }),
+      });
 
-    const data = await response.json();
-    console.log(data.result.random.data);
-    if (data.result.random.data[0] === 1) setResult("Heads");
-    if (data.result.random.data[0] === 2) setResult("Tails");
+      const data = await response.json();
+      if (data.result.random.data[0] === 1) setResult("Heads");
+      if (data.result.random.data[0] === 2) setResult("Tails");
+    } catch (error) {
+      console.log("Error: ", error);
+      setResult("Error");
+    }
   }
+
   return (
     <div>
       <div>
