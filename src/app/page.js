b806d6f8c0e690daebaa497c7d08e5ed04a8b1b0
oleknap-dev <aleksander.napiorkowski.an@gmail.com>
2025-02-06 15:44:37 +0100

    Improve UI styling

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import Tile from "./components/Tile";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center mr-6">
+    <div className="flex justify-center items-center gap-2">
       <Tile
         label="Random Number"
         href="/random-number"
         image="/images/random-numbers.jpg"
       />
-      <Tile label="Coin flip" href="/coin-flip" />
+      <Tile label="Coin flip" href="/coin-flip" image="/images/coin-flip.jpg" />
     </div>
   );
 }
