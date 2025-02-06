3f3fff57fdbecc920ee00ec16a615712a7dd95b1
oleknap-dev <aleksander.napiorkowski.an@gmail.com>
2025-02-06 15:56:22 +0100

    Add Home Page header and style it

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,22 @@ import Tile from "./components/Tile";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center gap-2">
-      <Tile
-        label="Random Number"
-        href="/random-number"
-        image="/images/random-numbers.jpg"
-      />
-      <Tile label="Coin flip" href="/coin-flip" image="/images/coin-flip.jpg" />
+    <div>
+      <header>
+        <h1 className="text-7xl font-bold ml-12 mt-8 mb-24">Randomizer</h1>
+      </header>
+      <div className="flex justify-center items-center gap-2">
+        <Tile
+          label="Random Number"
+          href="/random-number"
+          image="/images/random-numbers.jpg"
+        />
+        <Tile
+          label="Coin flip"
+          href="/coin-flip"
+          image="/images/coin-flip.jpg"
+        />
+      </div>
     </div>
   );
 }
